3f1c629439c9fc50a863c932661c30ddb4efe202
George Cook <george@tantawowa.com>
2020-05-02 16:06:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+<p align="center">
+  <!--<img src="images/logo.png" alt="rLog logo" width="200" height="200"/>-->
+  roku ioc example
+</p>
+<h3 align="center">
+Super efficient list rendering, with dynamic cells, and animations
+</h3>
+
+## taken from maestro framework
+
+For more like this, check out the [maestro framework](https://github.com/georgejecook/maestro) for roku.
+
+Maestro is a framework that:
+
+ - uses brightscript like syntax and idioms
+ - with all the efficiencies and benefits of the modern brighterscript language
+ - making it possible to write in a more mature, and design pattern favouring coding style
+ - similar to typescript
+ - building off a rock solid roku-centric base
+ - which is intuitive for both seasoned scenegraph developers and typescript developers
+ - and takes away as much boilerplate as possible
+ - so developers spend their time solving their code problem, and not on roku nuances
+ - all the while conforming to best of breed roku patterns, to ensure performance and stability
+ - while increasing testabillity
+ - and capturing as many typos/bugs as possible at compile time
+
+
+## This project is written in brighterscript
+
+Brighterscript is a fresh, fun, friendly, exciting language we have used at applicaster, and other places for nearly a year. VScode supports brighterscript at the time of writing; and the official compiler is almost finished. 
+
+## but you will not be using brighterscript here, if you don't want to
+
+You can simply copy the tranpsiled brs sources from the build or dist folders.
+
+## Usage
+
+### Run from brighterscript
+
+- npm install the main folder
+- change `.vscode/.env` to point to your roku's ip and change the password
+- run from vscode, either run or test targets
+- Copy the build/source/maestro folder into your project's source folder (i.e you should have source/maestro, in your project).
+
+### Just grab sources and go
+
+- Copy the dist/source/maestro folder into your project's source folder (i.e you should have source/maestro, in your project).
+
+## API
+
+Coming soon (seriously; I document like crazy, this will get done; sharing early for some chums to play with)
---
+++ gulpfile.ts
@@ -0,0 +1,150 @@
+import { series } from "gulp";
+import { RooibosProcessor, createProcessorConfig } from 'rooibos-cli';
+import { BurpConfig, BurpProcessor } from "burp-brightscript";
+import { MaestroProjectProcessor, createMaestroConfig } from 'maestro-cli-roku';
+
+const gulp = require('gulp');
+const gulpClean = require('gulp-clean');
+const outDir = './build';
+const rokuDeploy = require('roku-deploy');
+
+let args = {
+  host: process.env.ROKU_HOST || '192.168.16.3',
+  username: process.env.ROKU_USER || 'rokudev',
+  password: process.env.ROKU_PASSWORD || 'aaaa',
+  rootDir: './build',
+  files: ['**/*'],
+  outDir: './out',
+  retainStagingFolder: true
+};
+
+export function clean() {
+  console.log('Doing a clean at ' + outDir);
+  return gulp.src(['out', outDir], { allowEmpty: true }).pipe(gulpClean({ force: true }));
+}
+
+export function createDirectories() {
+  return gulp.src('*.*', { read: false })
+    .pipe(gulp.dest('./build'))
+    .pipe(gulp.dest('./out'));
+}
+
+export async function deploy(cb) {
+  await rokuDeploy.publish(args);
+}
+
+async function compile(cb) {
+  let config = createMaestroConfig({
+    filePattern: ['**/*.bs', '**/*.brs', '**/*.xml'],
+    sourcePaths: ['./src'],
+    outputPath: outDir,
+    logLevel: 2,
+    processingExcludedPaths: [
+      'components/rLogComponents/**/*.*',
+      'source/rLog/**/*.*',
+      'source/tests/rooibosDist.brs',
+      'source/tests/rooibosFunctionMap.brs',
+      'source/AdobeAccessEnabler/adobeAuthorizationTemplate.xml'
+    ],
+    nonCheckedImports: [
+      'source/zapp.brs',
+      'source/MRuntime.brs',
+      'source/rLog/rLogMixin.brs',
+      'source/tests/rooibosDist.brs',
+      'source/rooibosFunctionMap.brs'
+    ]
+  });
+  let processor = new MaestroProjectProcessor(config);
+  await processor.processFiles();
+}
+
+async function prepareTests(cb) {
+
+  let testFiles = [];
+
+  if (process.env.TEST_FILES_PATTERN) {
+    console.log('using overridden test files');
+    testFiles = JSON.parse(process.env.TEST_FILES_PATTERN);
+  } else {
+    testFiles = [
+      '**/tests/**/*.brs',
+      '!**/rooibosDist.brs',
+      '!**/rooibosFunctionMap.brs',
+      '!**/TestsScene.brs'
+    ];
+  }
+
+  const isCodeCoverageBuild = false;
+
+  let config = createProcessorConfig({
+    projectPath: outDir,
+    sourceFilePattern: [
+      '**/*.brs',
+      '**/*.xml',
+      '!**/tests/**/*.*',
+      '!**/tests',
+      '!**/rLog',
+      '!**/rLog/**/*.*',
+      '!**/rLogComponents',
+      '!**/rLogComponents/**/*.*',
+      '!**/rooibosDist.brs',
+      '!**/rooibosFunctionMap.brs',
+      '!**/TestsScene.brs',
+      '!**/ThreadUtils.brs'
+    ],
+    showFailuresOnly: true,
+    testsFilePattern: testFiles,
+    isRecordingCodeCoverage: isCodeCoverageBuild
+  });
+  let processor = new RooibosProcessor(config);
+  processor.processFiles();
+
+  cb();
+}
+export function applyBurpPreprocessing(cb) {
+  const currentPath = process.cwd();
+  let replacements = null;
+  if (process.env.buildType === 'prod') {
+    replacements = [{
+      regex: '(^\\s*(m\\.)*(registerLogger)\\((\\s*"))',
+      replacement: '\'$1'
+    },
+    {
+      regex: '(^\\s*(m\\.)*(logMethod|logInfo|logError|logVerbose|logDebug|logWarn)\\()',
+      replacement: '\'$1'
+    },
+
+    ];
+  } else {
+    replacements = [
+      {
+        regex: '(^\\s*(m\\.)*(logInfo|logError|logVerbose|logDebug|logWarn|logMethod)\\((\\s*"))',
+        replacement: '$1#FullPath# '
+      }
+    ];
+  }
+  
+  // add crash resilience to tests
+  if (process.env.buildType === 'test') {
+    replacements.push({
+      regex: '(^(\\s*)m\\.assert)',
+      replacement: '  if not m.currentResult.isFail then $1'
+    });
+  }
+  let config: BurpConfig = {
+    sourcePath: outDir,
+    globPattern: '**/*.brs',
+    replacements: replacements
+  };
+
+  const processor = new BurpProcessor(config);
+  processor.processFiles();
+  console.log(`Resetting path to ${currentPath}`);
+  process.chdir(currentPath);
+  cb();
+}
+
+
+exports.build = series(clean, createDirectories, compile, applyBurpPreprocessing);
+exports.prePublish = series(exports.build)
+exports.prePublishTests = series(exports.build, prepareTests);
